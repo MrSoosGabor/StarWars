77c8488c0874cc9804040aa8f878a58d5569f8e5
Soós Gábor <soos.gabor@jedlik.eu>
2021-03-18 09:50:44 +0100

    A pálya és az űrhajó kirajzolása.

---
+++ StarWars.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+def balra():
+    pass
+
+
+space = turtle.Screen()
+space.setup(width=800, height=600)
+space.bgpic("background.png")
+space.addshape("sprite.gif")
+space.onkeypress(balra, "Left")
+
+
+ship = turtle.Turtle()
+ship.shape("sprite.gif")
+
+while True:
+
+    space.update()
